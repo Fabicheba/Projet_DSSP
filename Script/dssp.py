18a11a490b9aa2a812196b55545b902b2964eb69
Fabisha <fatoumatabinta.barry@outlook.fr>
2020-09-15 23:32:18 +0200

    Premiere version de la fonction distance et calcul energie

--- Script/dssp.py
+++ Script/dssp.py
@@ -1,4 +1,5 @@
 import sys
+import math
 def extraction_atome(fichier_pdb, fichier_text):
     """ Lit un fichier pdb et renvoie les coordonées de tous les atomes
         impliqués dans la liaison peptidique (C,O,H,N).
@@ -15,14 +16,11 @@ def extraction_atome(fichier_pdb, fichier_text):
     fichier_text
 
     """
-    modele = 1
-    atome = 0
     nom = ["C","O","N","H"]
     with open(fichier_pdb, "r") as pdb, open(fichier_text, "w") as text:
         for ligne in pdb:
             if ligne.startswith("ATOM") and ligne[12:16].strip() in nom:
-                atome += 1
-                posi=ligne[24:27].strip()
+                posi=ligne[22:27].strip()
                 nam=ligne[12:16].strip()
                 resid=ligne[17:20].strip()
                 x = ligne[32:38]
@@ -44,12 +42,81 @@ def dico_coord(file_text):
 			if posi in dico:
 				dico[posi].append((res, at, x,y,z))
 				dico[posi] = [(res, at, x, y, z)]
-	return dico, position
+	return dico
+
+def distance(res1, res2):
+    x1, y1, z1=float(res1[0]), float(res1[1]), float(res1[2])
+    x2, y2, z2=float(res2[0]), float(res2[1]), float(res2[2])
+    norme = (x1-x2)**2 + (y1-y2)**2 + (z1-z2)**2
+    distance=math.sqrt(norme)
+    return distance
+
+
+def calcul_energie(dico):
+    cle=list(dico.keys())
+    val=list(dico.values())
+    q1 = -0.42
+    q2 = 0.28
+    f = 332
+    liaison = {}
+    for i in range(len(cle)-1):
+        res1=dico[cle[i]]
+        print(res1)
+        C=res1[1][2:]
+        O=res1[2][2:]
+        #print(C,O)
+        #xc,yc,zc=float(C[2]), float(C[3]), float(C[4])
+        #xo,yo,zo=float(O[2]), float(O[3]), float(O[4])
+        for j in range(i+1,len(cle)):
+            res2 = dico[cle[j]]
+            N = res2[0][2:]
+            if len(res2)!=5:
+            	H=(0,0,0)
+            else:
+            	H = res2[3][2:]
+            ron=distance(O,N)
+            rch=distance(C,H)
+            roh=distance(O,H)
+            rcn=distance(C,N)
+            energie=q1*q2*f*(1/ron + 1/rch + 1/roh + 1/rcn)
+            if energie < -0.5:
+                liaison[(i,j)] = energie
+    return liaison
+
+
+
+
 
 if __name__ == '__main__':
-    file_pdb = sys.argv[1]
-    file_text = sys.argv[2]
-    extraction_atome(file_pdb,file_text)
-    dico, position=dico_coord(file_text)
-    print(position[:10])    
+	file_pdb = sys.argv[1]
+	file_text = sys.argv[2]
+	extraction_atome(file_pdb,file_text)
+	dico=dico_coord(file_text)
+	lh=calcul_energie(dico)
+	print(lh)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
